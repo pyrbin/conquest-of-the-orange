8a765a7c2ed416bbbdb4fd758b57435fe1533b62
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-05-08 22:45:20 +0200

    Added painter & test scene

---
+++ Assets/Scripts/Painting/Painter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Painter : MonoBehaviour
+{
+    [Serializable]
+    public struct PainterData
+    {
+        public int Radius;
+        public Color Color;
+    }
+
+    public PainterData PaintingData;
+    public PaintableSurfaceTexture Surface;
+
+    [HideInInspector]
+    public float RadiusMult = 1f;
+
+    public float2 WorldPos2D => new float2(transform.position.x, transform.position.y);
+
+    public void Update()
+    {
+        // TODO: Remove
+        if (Input.GetMouseButton(0))
+        {
+            Paint(Surface);
+        }
+    }
+
+    public void Paint(PaintableSurfaceTexture surface)
+    {
+        surface.PaintSurface(new PaintSurfaceInfo
+        {
+            WorldPos = WorldPos2D,
+            Radius = (int)(PaintingData.Radius * RadiusMult),
+            Color = PaintingData.Color
+        });
+    }
+}
