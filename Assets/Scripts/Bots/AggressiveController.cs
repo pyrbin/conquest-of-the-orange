e3050d889035b3454a8c7e519be2495b8c5f211d
smultron <isafr132@student.liu.se>
2020-05-09 19:26:52 +0200

    Added two controllers.

---
+++ Assets/Scripts/Bots/AggressiveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AggressiveController : MonoBehaviour
+{
+
+    public FruitEntity FruitEntity { get; private set; }
+
+    [HideInInspector]
+    public Vector3 TargetPos { get; private set; }
+
+    private Rigidbody2D player;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        FruitEntity = GetComponentInChildren<FruitEntity>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float xOffset = Random.Range(-1.5f, 1.5f);
+        float yOffset = Random.Range(-1.5f, 1.5f);
+        Vector2 pos = new Vector2(player.position.x + xOffset, player.position.y + yOffset);
+        FruitEntity.Movement.MoveTo(pos);
+    }
+
+
+}
--- Assets/Scripts/Bots/PredeterminedController.cs
+++ Assets/Scripts/Bots/PredeterminedController.cs
@@ -1,21 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Unity.Mathematics;
 using Random = UnityEngine.Random;
 
-public class RandomController : MonoBehaviour
+public class PredeterminedController : MonoBehaviour
 {
     public FruitEntity FruitEntity { get; private set; }
+    public List<Vector2> points;
 
     [HideInInspector]
     public Vector3 TargetPos { get; private set; }
 
-    private LevelManager levelManager;
+    private int index = 0;
+    private float height = 4.6f;
+    private float width = 8.5f;
 
     // Start is called before the first frame update
     private void Awake()
     {
+;
+
+        if (points == null || points.Count == 0)
+        {
+            print("lmao");
+            points.Add(new Vector2(-8, -4));
+            points.Add(new Vector2(8, -4));
+            points.Add(new Vector2(8, 4));
+            points.Add(new Vector2(-8, 4));
+            points.Add(new Vector2(0, 0));
+        }
+   
+        foreach (Vector2 point in points)
+        {
+            if (math.abs(point.x) > width)
+                print("Absolute value of x must be less than " + width);
+            if (math.abs(point.y) > width)
+                print("Absolute value of y must be less than " + height);
+        }
+
         TargetPos = getNewPosition();
         FruitEntity = GetComponentInChildren<FruitEntity>();
+
     }
 
     // Update is called once per frame
@@ -26,23 +52,23 @@ public class RandomController : MonoBehaviour
         FruitEntity.Movement.MoveTo(TargetPos);
     }
 
-    private Vector2 getNewPosition() {
-        float x = Random.Range(levelXBounds().x, levelXBounds().y);
-        float y = Random.Range(levelYBounds().x, levelYBounds().y);
-        return new Vector2(x, y);
+    private Vector2 getNewPosition()
+    {
+        index++;
+        return points[index % points.Count];
     }
 
     private float2 levelXBounds()
     {
         LevelInfo levelInfo = Game.Find().LevelManager.GetCurrentLevel();
-        float width = 8.5f;
+
         return new Vector2((levelInfo.Pos.x - width), (levelInfo.Pos.x + width));
     }
 
     private float2 levelYBounds()
     {
         LevelInfo levelInfo = Game.Find().LevelManager.GetCurrentLevel();
-        float height = 4.6f;
+
         return new Vector2((levelInfo.Pos.y - height), (levelInfo.Pos.y + height));
     }
 
